8c548ce9eebcf5c04409f95f8e00b4f90b1ced91
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2023-12-14 15:56:37 +0530

    add auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Layout = ({children} : {children : React.ReactNode}) => {
+  return (
+    <main className='flex items-center justify-center w-full min-h-screen'>
+      {children}
+    </main>
+  )
+}
+
+export default Layout
